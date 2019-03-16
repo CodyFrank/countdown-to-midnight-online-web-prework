47b17a63b7ee54b5101f7b55b241aef20c039603
Cody Frank <cody.frank30@gmail.com>
2019-03-16 01:11:18 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,18 @@
 #write your code here
 
-def countdown
+def countdown (int)
+  while int > 0
+    puts "#{int} SECOND(S)!"
+    int -= 1
+  end
+  "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep (index)
+  while index > 0
+    puts "#{index} SECONDS(S)!"
+    index -= 1
+    sleep(1)
+  end
+  "HAPPY NEW YEAR!"
 end
